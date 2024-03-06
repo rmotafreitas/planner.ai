a35a88f5c07ba5879b9669e6d3b9b7177b06aef9
RICARDO FREITAS <rmotafreitas@gmail.com>
2024-03-06 13:50:01 +0000

    Setup shadcdn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,50 @@
+import { MapPin, Wallet, Calendar } from "@phosphor-icons/react";
+import { Button } from "@/components/ui/button";
+
 export function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="rounded-md w-fit flex flex-row gap-4">
+        <div className="flex flex-row gap-2 justify-evenly flex-1">
+          <div className="border-solid border-2 border-purple-300 justify-center flex items-center rounded-md w-12 h-12">
+            <MapPin size={32} weight="fill" color="purple" />
+          </div>
+          <div>
+            <h1 className="text-sm">Destination</h1>
+            <p className="font-semibold">San Francisco, CA</p>
+          </div>
+        </div>
+        <div className="flex flex-row gap-2 justify-evenly flex-1">
+          <div className="border-solid border-2 border-purple-300 justify-center flex items-center rounded-md w-12 h-12">
+            <MapPin size={32} weight="fill" color="purple" />
+          </div>
+          <div>
+            <h1 className="text-sm">Destination</h1>
+            <p className="font-semibold">San Francisco, CA</p>
+          </div>
+        </div>
+        <div className="flex flex-row gap-2 justify-evenly flex-1">
+          <div className="border-solid border-2 border-purple-300 justify-center flex items-center rounded-md w-12 h-12">
+            <Wallet size={32} weight="fill" color="purple" />
+          </div>
+          <div>
+            <h1 className="text-sm">Price Range</h1>
+            <p className="font-semibold">$800-$1000</p>
+          </div>
+        </div>
+        <div className="flex flex-row gap-2 justify-evenly flex-1">
+          <div className="border-solid border-2 border-purple-300 justify-center flex items-center rounded-md w-12 h-12">
+            <Calendar size={32} weight="fill" color="purple" />
+          </div>
+          <div>
+            <h1 className="text-sm">Date</h1>
+            <p className="font-semibold">$800-$1000</p>
+          </div>
+        </div>
+        <Button>
+          <p>Explore Now!</p>
+        </Button>
+      </div>
     </>
   );
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(
+        "C:/Users/Ricardo Freitas/Documents/Projetos/UPDATE2324/client/src"
+      ),
+    },
+  },
+});
