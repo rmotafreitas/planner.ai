0a7e0b17c4cf74aa9ffff794ece602b2d4879325
RICARDO FREITAS <rmotafreitas@gmail.com>
2024-03-06 23:07:36 +0000

    Update generate.ts: Remove prompt parameter and retrieve prompt from database

--- server/src/routes/completion/gen/generate.ts
+++ server/src/routes/completion/gen/generate.ts
@@ -8,11 +8,19 @@ import { openai, streamRes } from "../../../lib/opeanai";
 export async function completion(request: FastifyRequest, reply: FastifyReply) {
   const bodySchema = z.object({
     tripId: z.string(),
-    prompt: z.string(),
+    // prompt: z.string(),
     temperature: z.number().min(0).max(1).default(0.5),
   });
 
-  const { tripId, prompt, temperature } = bodySchema.parse(request.body);
+  const { tripId, /* prompt, */ temperature } = bodySchema.parse(request.body);
+  let promptData = await prisma.prompt.findFirst();
+  let prompt: string = "";
+
+  if (!promptData) {
+    return reply.status(400).send({ error: "Prompt not found" });
+  } else {
+    prompt = promptData.template;
+  }
 
   const trip = await prisma.trip.findUniqueOrThrow({
     where: {
